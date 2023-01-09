bb3c4b36c1f57e9bd5cdbcf4883ffa1075a1b9fe
Grant Ramsay <seapagan@gmail.com>
2023-01-09 20:01:44 +0000

    add unstyled input box to choose repo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import GitHubReadme from "./components/github-readme/GitHubReadme";
 
 function App() {
+  const [repoName, setRepoName] = useState("");
+  const [searchRepo, setSearchRepo] = useState("");
+
+  const apply = e => {
+    e.preventDefault();
+    if (!repoName) return;
+    setSearchRepo(repoName);
+  };
+
   return (
     <div className="App">
-      <GitHubReadme repo="seapagan/fastapi-template" branch="main" />
+      <form>
+        <input
+          type="text"
+          autoFocus={true}
+          placeholder="Enter a repository as username/repository"
+          value={repoName}
+          onChange={e => setRepoName(e.target.value)}
+        />
+        <button type="submit" onClick={apply}>
+          Get the README
+        </button>
+      </form>
+      {searchRepo && <GitHubReadme repo={searchRepo} branch="main" />}
     </div>
   );
 }
